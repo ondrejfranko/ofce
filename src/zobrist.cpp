9ec9f0afcd622ce344e0cffbfc8c9d33c76ebd08
Ondrej Franko <ondrejfranko02@gmail.com>
2026-05-23 23:45:01 +0200

    Skip initialising unused Zobrist keys

--- src/zobrist.cpp
+++ src/zobrist.cpp
@@ -11,12 +11,12 @@ void init_zobrist() {
     std::mt19937_64 prng(69420);
     std::uniform_int_distribution<Key> uniform_dist;
 
-    for (int piece = 0; piece < PIECE_COUNT; ++piece) {
-        for (int square = 0; square < SQUARE_COUNT; ++square) {
+    for (int piece = WHITE_PAWN; piece < PIECE_COUNT; ++piece) {
+        for (int square = A1; square < SQUARE_COUNT; ++square) {
             ZOBRIST_PSQ[piece][square] = uniform_dist(prng);
         }
     }
-    for (int file = 0; file < FILE_COUNT; ++file) {
+    for (int file = FILE_A; file < FILE_COUNT; ++file) {
         ZOBRIST_EP[file] = uniform_dist(prng);
     }
     for (int i = 0; i < 16; ++i) {
