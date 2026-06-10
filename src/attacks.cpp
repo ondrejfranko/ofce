2a0eb1450838d8246e96809c8e8b4941284ca390
Ondrej Franko <ondrejfranko02@gmail.com>
2026-06-10 21:40:17 +0200

    Add sliding mask creation helper function

--- src/attacks.cpp
+++ src/attacks.cpp
@@ -23,3 +23,55 @@ Bitboard BETWEEN_BB[SQUARE_COUNT][SQUARE_COUNT];
 
 // Squares of a line given by two squares
 Bitboard LINE_BB[SQUARE_COUNT][SQUARE_COUNT];
+
+// Generate bishop mask for a given square
+Bitboard get_bishop_mask(Square sq) {
+    Bitboard mask = 0;
+    int file = sq % 8;
+    int rank = sq / 8;
+
+    for (auto dir : {NORTH_EAST, NORTH_WEST, SOUTH_EAST, SOUTH_WEST}) {
+        int f = file + dir % 8;
+        int r = rank + dir / 8;
+
+        while (FILE_A <= f && f < FILE_COUNT && RANK_1 <= r && r < RANK_COUNT) {
+            int next_f = f + dir % 8;
+            int next_r = r + dir / 8;
+
+            if (FILE_A <= next_f || next_f < FILE_COUNT || RANK_1 <= next_r || next_r < RANK_COUNT) {
+                break;
+            }
+
+            mask |= bit(static_cast<Square>(r * 8 + f));
+            f = next_f;
+            r = next_r;
+        }
+    }
+    return mask;
+}
+
+// Generate rook mask for a given square
+Bitboard get_rook_mask(Square sq) {
+    Bitboard mask = 0;
+    int file = sq % 8;
+    int rank = sq / 8;
+
+    for (auto dir : {NORTH, SOUTH, WEST, EAST}) {
+        int f = file + dir % 8;
+        int r = rank + dir / 8;
+
+        while (FILE_A <= f && f < FILE_COUNT && RANK_1 <= r && r < RANK_COUNT) {
+            int next_f = f + dir % 8;
+            int next_r = r + dir / 8;
+
+            if (FILE_A <= next_f || next_f < FILE_COUNT || RANK_1 <= next_r || next_r < RANK_COUNT) {
+                break;
+            }
+
+            mask |= bit(static_cast<Square>(r * 8 + f));
+            f = next_f;
+            r = next_r;
+        }
+    }
+    return mask;
+}
