42673a2fbb005c2be4c5f9d710e250eab447bf6f
Ondrej Franko <ondrejfranko02@gmail.com>
2026-06-17 22:07:35 +0200

    Add all sliding lookup tables creation to attacks init

--- src/attacks.cpp
+++ src/attacks.cpp
@@ -217,4 +217,19 @@ void init_attacks() {
 
         KING_ATTACKS_BB[sq] = king_attacks;
     }
+
+    // Sliding masks and attacks
+    for (int sq = A1; sq < SQUARE_COUNT; ++sq) {
+        // Bishop mask
+        BISHOP_MASKS_BB[sq] = 0;
+        BISHOP_MASKS_BB[sq] |= get_bishop_mask(static_cast<Square>(sq));
+        // Build bishop attacks for all occupancy variations
+        build_bishop_attacks(static_cast<Square>(sq));
+
+        // Rook mask
+        ROOK_MASKS_BB[sq] = 0;
+        ROOK_MASKS_BB[sq] |= get_rook_mask(static_cast<Square>(sq));
+        // Build rook attacks for all occupancy variations
+        build_rook_attacks(static_cast<Square>(sq));
+    }
 }
