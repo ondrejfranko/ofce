697a4f79134bef0273a14bc94437b4e5b185c66c
Ondrej Franko <ondrejfranko02@gmail.com>
2026-07-18 18:05:01 +0200

    Implement pawn moves generation

--- src/movegen.cpp
+++ src/movegen.cpp
@@ -1,4 +1,5 @@
 #include "movegen.hpp"
+#include "attacks.hpp"
 #include "bitboard.hpp"
 #include "position.hpp"
 #include "types.hpp"
@@ -43,5 +44,112 @@ void generate_moves(const Position &pos, MoveList &list, GenType gen_type) {
         generate_king_moves<BLACK>(pos, list, target);
         if (gen_type != GenType::CAPTURES)
             generate_castling_moves<BLACK>(pos, list);
+    }
+}
+
+// Pawn move generation
+template <Color C>
+void generate_pawn_moves(const Position &pos, MoveList &list, Bitboard target) {
+    constexpr Piece PAWN_PIECE = make_piece(PAWN, C);
+    const Bitboard pawns = pos.piece_BB[PAWN_PIECE];
+    const Bitboard empty = ~(pos.color_BB[WHITE] | pos.color_BB[BLACK]);
+
+    Bitboard push_1_BB, push_2_BB, cap_left_BB, cap_right_BB;
+    Bitboard promo_rank, double_rank;
+    Direction push_dir, cap_left_dir, cap_right_dir;
+
+    if constexpr (C == WHITE) {
+        push_1_BB = shift_N(pawns) & empty;
+        push_2_BB = shift_N(shift_N(pawns & RANK_BB[RANK_2]) & empty) & empty;
+        cap_left_BB = shift_NW(pawns);
+        cap_right_BB = shift_NE(pawns);
+        promo_rank = RANK_BB[RANK_8];
+        double_rank = RANK_BB[RANK_4];
+        push_dir = NORTH;
+        cap_left_dir = NORTH_WEST;
+        cap_right_dir = NORTH_EAST;
+    } else {
+        push_1_BB = shift_S(pawns) & empty;
+        push_2_BB = shift_S(shift_S(pawns & RANK_BB[RANK_7]) & empty) & empty;
+        cap_left_BB = shift_SW(pawns);
+        cap_right_BB = shift_SE(pawns);
+        promo_rank = RANK_BB[RANK_1];
+        double_rank = RANK_BB[RANK_5];
+        push_dir = SOUTH;
+        cap_left_dir = SOUTH_WEST;
+        cap_right_dir = SOUTH_EAST;
+    }
+
+    // regular (non-promotion) pushes
+    {
+        Bitboard bb = push_1_BB & target & ~promo_rank;
+        while (bb) {
+            const Square to = pop_lsb(bb);
+            list.add_move(Move(static_cast<Square>(to - push_dir), to));
+        }
+    }
+
+    // double pushes
+    {
+        Bitboard bb = push_2_BB & target;
+        while (bb) {
+            const Square to = pop_lsb(bb);
+            list.add_move(Move(static_cast<Square>(to - push_dir - push_dir), to));
+        }
+    }
+
+    // promotion pushes
+    {
+        Bitboard bb = push_1_BB & target & promo_rank;
+        while (bb) {
+            const Square to = pop_lsb(bb);
+            const Square from = static_cast<Square>(to - push_dir);
+            for (const PieceType pt : {KNIGHT, BISHOP, ROOK, QUEEN})
+                list.add_move(Move(from, to, pt));
+        }
+    }
+
+    // regular (non-promotion) captures (including en passant)
+    {
+        Bitboard bb = cap_left_BB & target & ~promo_rank;
+        while (bb) {
+            const Square to = pop_lsb(bb);
+            const Square from = static_cast<Square>(to - cap_left_dir);
+            if (to == pos.en_passant_square)
+                list.add_move(Move(from, to, EP_MOVE));
+            else
+                list.add_move(Move(from, to));
+        }
+    }
+    {
+        Bitboard bb = cap_right_BB & target & ~promo_rank;
+        while (bb) {
+            const Square to = pop_lsb(bb);
+            const Square from = static_cast<Square>(to - cap_right_dir);
+            if (to == pos.en_passant_square)
+                list.add_move(Move(from, to, EP_MOVE));
+            else
+                list.add_move(Move(from, to));
+        }
+    }
+
+    // promotion captures
+    {
+        Bitboard bb = cap_left_BB & target & promo_rank;
+        while (bb) {
+            const Square to = pop_lsb(bb);
+            const Square from = static_cast<Square>(to - cap_left_dir);
+            for (const PieceType pt : {KNIGHT, BISHOP, ROOK, QUEEN})
+                list.add_move(Move(from, to, pt));
+        }
+    }
+    {
+        Bitboard bb = cap_right_BB & target & promo_rank;
+        while (bb) {
+            const Square to = pop_lsb(bb);
+            const Square from = static_cast<Square>(to - cap_right_dir);
+            for (const PieceType pt : {KNIGHT, BISHOP, ROOK, QUEEN})
+                list.add_move(Move(from, to, pt));
+        }
     }
 }
