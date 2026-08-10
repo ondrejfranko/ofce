c0e57be28938814253adfefb88938a5e3d44f790
Ondrej Franko <ondrejfranko02@gmail.com>
2026-08-10 14:07:04 +0200

    Implement auxiliary move making functions

--- src/makemove.cpp
+++ src/makemove.cpp
@@ -1,6 +1,11 @@
 #include "makemove.hpp"
+#include "bitboard.hpp"
+#include "movegen.hpp"
+#include "position.hpp"
 #include "types.hpp"
+#include "zobrist.hpp"
 #include <array>
+#include <cassert>
 #include <cstdint>
 
 // the castling rights that remain after the square has been the origin or destination of a move
@@ -14,4 +19,15 @@ constexpr std::array<int8_t, SQUARE_COUNT> build_castling_rights_mask() {
     mask[H8] = static_cast<int8_t>(CR_ALL & ~CR_BLACK_KINGSIDE);
     mask[A8] = static_cast<int8_t>(CR_ALL & ~CR_BLACK_QUEENSIDE);
     return mask;
+}
+
+constexpr auto CASTLING_RIGHTS_MASK = build_castling_rights_mask();
+
+bool is_king_attacked(const Position &pos, Color us) {
+    const Square ksq = get_lsb(pos.piece_BB[make_piece(KING, us)]);
+    return (us == WHITE) ? is_square_attacked<BLACK>(pos, ksq) : is_square_attacked<WHITE>(pos, ksq);
+}
+
+bool is_legal_move(const Position &pos, Move move) {
+    return !is_king_attacked(make_move(pos, move), pos.side_to_move);
 }
